da31799ded85d19cea4c44d0e11237b3fb921134
Bernabe <bernabe@maniak.com.mx>
2018-07-29 18:02:00 -0500

    Add found category detail mobile styles

---
+++ wp-content/themes/20broad/inc/components/custom/found_category_detail.php
@@ -0,0 +1,41 @@
+<div class="image-side">
+
+</div>
+
+<div class="content-side">
+    <div class="number">
+        01
+    </div>
+
+    <div class="custom-logo">
+        <img src="http://found.test/wp-content/uploads/2018/07/logo_desktop.png" alt="">
+    </div>
+
+    <div class="body paragraph">
+        Oh, the people you’ll meet…
+
+        FOUND Hotels are full of character and cater to modern day explorers
+        who expect consistent social and local community experiences while
+        traveling on a budget.
+        Each location is centrally located and offers shared bunk and private room accommodations.
+        Stylish common areas, daily activities and unique food & beverage access
+        provide further opportunities for our guests to immerse themselves and connect.
+        As part of our loyalty program, you will gain access to other Found Hotels,
+        Found Residences and Partners.
+    </div>
+
+    <div class="locations-lists">
+        <div class="locations-list-wrapper">
+            <h4 class="headline4 title">Current Locations:</h4>
+            <ul class="locations-list">
+                <li class="headline4">Chicago</li>
+                <li class="headline4">Boston</li>
+                <li class="headline4">Washington D.C.</li>
+                <li class="headline4">San Diego</li>
+            </ul>
+        </div>
+    </div>
+    <div class="link-wrapper">
+        <a href="" class="found-link desert">Learn more about FOUND Study</a>
+    </div>
+</div>
