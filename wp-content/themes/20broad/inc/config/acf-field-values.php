e3fbe72ed2a80f60ddd65ba36c4adefc101dc928
Bernabe <bernabe@maniak.com.mx>
2018-07-29 13:59:02 -0500

    Add locations text

--- wp-content/themes/20broad/inc/config/acf-field-values.php
+++ wp-content/themes/20broad/inc/config/acf-field-values.php
@@ -35,6 +35,7 @@ function acf_load_cta_choices( $field ) {
 function acf_load_theme_choices( $field ) {
 	// Reset choices
 	$field['choices'] = array(
+        'white' => 'White',
 		'default' => 'Default',
 		'background' => 'Background',
 		'granny-smith' => 'Granny Smith'
