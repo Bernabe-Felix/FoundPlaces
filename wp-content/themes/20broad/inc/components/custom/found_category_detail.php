00b5efd736fb8b1cf01cd91d81beae1f685c3633
Bernabe <bernabe@maniak.com.mx>
2018-07-29 21:46:05 -0500

    Add category detail left side

--- wp-content/themes/20broad/inc/components/custom/found_category_detail.php
+++ wp-content/themes/20broad/inc/components/custom/found_category_detail.php
@@ -1,57 +1,66 @@
-<div class="image-side">
-
-</div>
-
-<div class="content-side">
-    <div class="number">
-        01
-    </div>
-
-    <div class="custom-logo">
-        <img src="http://found.test/wp-content/uploads/2018/07/logo_desktop.png" alt="">
-    </div>
-
-    <div class="body paragraph">
-        Oh, the people you’ll meet…
-
-        FOUND Hotels are full of character and cater to modern day explorers
-        who expect consistent social and local community experiences while
-        traveling on a budget.
-        Each location is centrally located and offers shared bunk and private room accommodations.
-        Stylish common areas, daily activities and unique food & beverage access
-        provide further opportunities for our guests to immerse themselves and connect.
-        As part of our loyalty program, you will gain access to other Found Hotels,
-        Found Residences and Partners.
-    </div>
-
-    <div class="locations-lists">
-        <div class="locations-list-wrapper">
-            <h4 class="headline4 title">Current Locations:</h4>
-            <ul class="locations-list">
-                <li class="headline4">Chicago</li>
-                <li class="headline4">Boston</li>
-                <li class="headline4">Washington D.C.</li>
-                <li class="headline4">San Diego</li>
-            </ul>
+<div class="grid pure-g">
+    <div class="pure-u-lg-6-12 image-side">
+        <div class="title-wrapper">
+            <p class="number">01</p>
+            <p class="headline2 title">FOUND Hotels</p>
         </div>
 
-        <div class="locations-list-wrapper">
-            <h4 class="headline4 title">Opening in 2019:</h4>
-            <ul class="locations-list">
-                <li class="headline4">San Francisco</li>
-                <li class="headline4">Los Angeles</li>
-            </ul>
-        </div>
-
-        <div class="locations-list-wrapper">
-            <h4 class="headline4 title">Opening in 2020:</h4>
-            <ul class="locations-list">
-                <li class="headline4">Waikiki</li>
-                <li class="headline4">New York</li>
-            </ul>
+        <div class="image-wrapper">
+            <img src="https://placebear.com/500/200" alt="">
         </div>
     </div>
-    <div class="link-wrapper">
-        <a href="" class="found-link desert">Learn more about FOUND Study</a>
+
+    <div class="pure-u-lg-6-12 content-side">
+        <div class="number">
+            01
+        </div>
+
+        <div class="custom-logo">
+            <img src="http://found.test/wp-content/uploads/2018/07/logo_desktop.png" alt="">
+        </div>
+
+        <div class="body paragraph">
+            Oh, the people you’ll meet…
+
+            FOUND Hotels are full of character and cater to modern day explorers
+            who expect consistent social and local community experiences while
+            traveling on a budget.
+            Each location is centrally located and offers shared bunk and private room accommodations.
+            Stylish common areas, daily activities and unique food & beverage access
+            provide further opportunities for our guests to immerse themselves and connect.
+            As part of our loyalty program, you will gain access to other Found Hotels,
+            Found Residences and Partners.
+        </div>
+
+        <div class="locations-lists pure-g">
+            <div class="pure-u-lg-4-12 locations-list-wrapper">
+                <h4 class="headline4 title">Current Locations:</h4>
+                <ul class="locations-list">
+                    <li class="headline4">Chicago</li>
+                    <li class="headline4">Boston</li>
+                    <li class="headline4">Washington D.C.</li>
+                    <li class="headline4">San Diego</li>
+                </ul>
+            </div>
+
+            <div class="pure-u-lg-4-12 locations-list-wrapper">
+                <h4 class="headline4 title">Opening in 2019:</h4>
+                <ul class="locations-list">
+                    <li class="headline4">San Francisco</li>
+                    <li class="headline4">Los Angeles</li>
+                </ul>
+            </div>
+
+            <div class="pure-u-lg-4-12 locations-list-wrapper">
+                <h4 class="headline4 title">Opening in 2020:</h4>
+                <ul class="locations-list">
+                    <li class="headline4">Waikiki</li>
+                    <li class="headline4">New York</li>
+                </ul>
+            </div>
+        </div>
+        <div class="link-wrapper">
+            <a href="" class="found-underline-link desert">Learn more about FOUND Study</a>
+        </div>
     </div>
 </div>
