9209ab08549dcd65d2489b13e68604620a014589
Bernabe <bernabe@maniak.com.mx>
2018-08-02 17:52:23 -0500

    Remove unused code

--- wp-content/themes/found-residences/inc/config/acf-field-values.php
+++ wp-content/themes/found-residences/inc/config/acf-field-values.php
@@ -2,51 +2,48 @@
 // Allows creation of predefined select/radio button options if specific field
 // names are being used
 function acf_load_button_color_field_choices( $field ) {
-	// reset choices
-	$field['choices'] = array(
-		'black' => 'Black',
-		'white' => 'White',
-	);
+    // reset choices
+    $field['choices'] = array(
+        'black' => 'Black',
+        'white' => 'White',
+    );
 
-	return $field;
+    return $field;
 }
 
 function acf_load_component_choices( $field ) {
-	$dir	= THEME_DIR . '/inc/components/custom/';
-	$allFiles = scandir($dir);
-	$files = array_diff($allFiles, array('.', '..', '.DS_Store'));
-	$field['choices'] = $files;
+    $dir	= THEME_DIR . '/inc/components/custom/';
+    $allFiles = scandir($dir);
+    $files = array_diff($allFiles, array('.', '..', '.DS_Store'));
+    $field['choices'] = $files;
 
-	return $field;
+    return $field;
 }
 
 function acf_load_cta_choices( $field ) {
-	// Reset choices
-	$field['choices'] = array(
-		'none' => 'No CTA',
-		'internal' => 'Internal Link CTA',
-		'external' => 'External Link CTA'
-	);
+    // Reset choices
+    $field['choices'] = array(
+        'none' => 'No CTA',
+        'internal' => 'Internal Link CTA',
+        'external' => 'External Link CTA'
+    );
 
 
-	return $field;
+    return $field;
 }
 
 function acf_load_theme_choices( $field ) {
-	// Reset choices
-	$field['choices'] = array(
-		'ebb' => 'Ebb',
+    // Reset choices
+    $field['choices'] = array(
+        'bone' => 'Bone',
+        'black' => 'Black',
         'white' => 'White',
-		'swirl' => 'Swirl',
-        'merino' => 'Merino',
-		'default' => 'Default',
-        'clam-shell' => 'Clam Shell',
-		'pearl-bush' => 'Pearl Bush',
-		'granny-smith' => 'Granny Smith',
-        'half-clam-shell-half-white' => 'Half Clam Shell - Half White',
-	);
+        'tan-hide' => 'Tan Hide',
+        'solitaire' => 'Solitaire',
+        'cutty-sark' => 'Cutty Sark'
+    );
 
-	return $field;
+    return $field;
 }
 
 // Preload shared select box & radio button values. 'name' is the field name
