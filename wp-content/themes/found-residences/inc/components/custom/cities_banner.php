33f0354eb986b127311623faa3f9bb955894c1b2
Bernabe <bernabe@maniak.com.mx>
2018-08-03 05:57:37 -0500

    Update residences cities banner

--- wp-content/themes/found-residences/inc/components/custom/cities_banner.php
+++ wp-content/themes/found-residences/inc/components/custom/cities_banner.php
@@ -1,18 +1,10 @@
 <div class="cities-banner">
+    <?php
+        foreach ($globalColumn['residences_cities_banner'] as $city){
+    ?>
     <div class="banner-item">
-        <h2 class="text-headline">Center City</h2>
-        <h3 class="text-subhead">Philadelphia</h3>
+        <h2 class="text-headline"><?= $city['location_name']; ?></h2>
+        <h3 class="text-subhead"><?= $city['city_name']; ?></h3>
     </div>
-    <div class="banner-item">
-        <h2 class="text-headline">UCLA</h2>
-        <h3 class="text-subhead">Los Angeles</h3>
-    </div>
-    <div class="banner-item">
-        <h2 class="text-headline">Downtown</h2>
-        <h3 class="text-subhead">Oakland (2019)</h3>
-    </div>
-    <div class="banner-item">
-        <h2 class="text-headline">Mid Market</h2>
-        <h3 class="text-subhead">San Francisco (2020)</h3>
-    </div>
+    <?php } ?>
 </div>
