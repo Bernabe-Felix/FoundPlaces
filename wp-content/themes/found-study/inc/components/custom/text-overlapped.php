5b62a69a4fac5e1f326589374b43d5aa680b4e67
Bernabe <bernabe@maniak.com.mx>
2018-08-03 06:42:28 -0500

    Update overlapped text for ACF

--- wp-content/themes/found-study/inc/components/custom/text-overlapped.php
+++ wp-content/themes/found-study/inc/components/custom/text-overlapped.php
@@ -1,13 +1,16 @@
 <div class="text-image-overlapped" style="background-color: #FEF4E4">
     <div class="paragraph" style="background-color: #FEF4E4">
-        Found Study is a proven / trusted low-cost provider of dedicated student living in dense urban cities where affordability and quality supply are constrained. Participating students and schools are welcome to graduate to the Found residential and hospitality network. Once you are in the family, we want you to stay.
+        <?= $globalColumn['overlapped_locations_body_text'] ?>
     </div>
 
     <div class="overlapped" style="background-color: #A3BFCC">
+<!--        overlapped_locations_lists-->
         <div class="overlapped-list">
+            <!--        overlapped_locations_list_title-->
             <p class="list-title">Current Locations</p>
 
             <ul class="">
+<!--                overlapped_location_name-->
                 <li class="list-item">San Francisco I</li>
                 <li class="list-item">Los Angeles / Hollywood I</li>
                 <li class="list-item">Los Angeles / Hollywood II</li>
