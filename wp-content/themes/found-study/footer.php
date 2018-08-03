730e84dd8df776f5284b1dd0a1c342b695a57aed
Bernabe <bernabe@maniak.com.mx>
2018-08-03 02:30:20 -0500

    Add styles for footer

--- wp-content/themes/found-study/footer.php
+++ wp-content/themes/found-study/footer.php
@@ -1,26 +1,26 @@
 </main>
 </div>
-<footer class="component-footer component-theme-merino fade-me-in">
+<footer class="component-footer component-theme-casper fade-me-in">
 	<div class="pure-g footer-content">
         <!--    Offset    -->
-        <div class="pure-u-sm-1-12 pure-u-lg-1-24"> </div>
+        <div class="pure-u-sm-1-12 pure-u-lg-2-24"> </div>
 		<div class="pure-u-sm-10-12 pure-u-lg-6-24 text headline2">
             We’re working on adding more information to this site. Please check back soon.
 		</div>
         <div class="pure-u-sm-1-12 pure-u-lg-2-24"> </div>
 
         <!--    Offset    -->
-        <div class="pure-u-sm-1-12 pure-u-lg-6-24"> </div>
+        <div class="pure-u-sm-1-12 pure-u-lg-5-24"> </div>
         <div class="pure-u-sm-10-12 pure-u-lg-8-24 text headline2">
             If you’d like to learn more about us, please email
             <div class="contact-link">
-                <a class="residences-link tan-hide" href="mailto:info@foundresidences.com">info@foundresidences.com</a>
+                <a class="study-link calypso" href="mailto:info@foundresidences.com">info@foundresidences.com</a>
             </div>
         </div>
 	</div>
 
     <div class="footer-bottom">
-        <span class="residences-link solitaire copyright">© 2018 FOUND Residences</span>
+        <span class="study-link timber-green copyright">© 2018 FOUND Residences</span>
 
         <div class="footer-menu">
             <?php
