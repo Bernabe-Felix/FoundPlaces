240e8dbf6454d7500f1339875d941218f464bb3f
Bernabe <bernabe@maniak.com.mx>
2018-08-02 20:05:16 -0500

    Add styles for footer mobile

--- wp-content/themes/found-residences/footer.php
+++ wp-content/themes/found-residences/footer.php
@@ -4,22 +4,23 @@
 	<div class="pure-g footer-content">
         <!--    Offset    -->
         <div class="pure-u-sm-1-12  "> </div>
-		<div class="pure-u-sm-10-12 pure-u-lg-3-12 text">
+		<div class="pure-u-sm-10-12 pure-u-lg-3-12 text headline2">
             We’re working on adding more information to this site. Please check back soon.
 		</div>
+        <div class="pure-u-sm-1-12"> </div>
 
         <!--    Offset    -->
-        <div class="pure-u-sm-2-12 pure-u-lg-3-12"> </div>
-        <div class="pure-u-sm-8-12 pure-u-lg-4-12 text">
+        <div class="pure-u-sm-1-12 pure-u-lg-3-12"> </div>
+        <div class="pure-u-sm-10-12 pure-u-lg-4-12 text headline2">
             If you’d like to learn more about us, please email
             <div class="contact-link">
-                <a class="found-link desert" href="mailto:info@foundresidences.com">info@foundresidences.com</a>
+                <a class="residences-link tan-hide" href="mailto:info@foundresidences.com">info@foundresidences.com</a>
             </div>
         </div>
 	</div>
 
     <div class="footer-bottom">
-        <span class="found-link timber-green copyright">© 2018 FOUND Residences</span>
+        <span class="residences-link solitaire copyright">© 2018 FOUND Residences</span>
 
         <div class="footer-menu">
             <?php
