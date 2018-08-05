6e10c7dd79ecf250701c577a306858a1f32faa53
Bernabe <bernabe@maniak.com.mx>
2018-08-05 18:59:18 -0500

    Add styles for pop up close

--- wp-content/themes/found-places/inc/components/custom/found_map.php
+++ wp-content/themes/found-places/inc/components/custom/found_map.php
@@ -7,6 +7,13 @@
         $mapText = 'FOUND Locations';
 //        $mapTextColor = $globalColumn['map_text_color'];
         $mapTextColor = '#B36A20';
+        $markers = $globalColumn['map_markers'];
+        $markerType = $markers[0]['marker_type'];
+        $markerX = 'marker_x';
+        $markerY = 'marker_y';
+        $markerTitle = 'marker_title';
+        $markerAddress = 'marker_address';
+        $markerPhone = 'marker_phone';
     ?>
     <h2 class="headline2 map-text" style="color: <?= $mapTextColor; ?>"><?= $mapText; ?> </h2>
 
@@ -28,6 +35,7 @@
             $phone = "(224) 243-6863";
         ?>
         <div class="marker-popup">
+            <span class="popup-close">×</span>
             <p class="marker-title"><?= $title; ?></p>
             <p class="marker-body"><?= $body; ?></p>
             <p class="marker-phone"><?= $phone; ?></p>
